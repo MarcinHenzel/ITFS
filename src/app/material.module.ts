fceed7d7b7c344a5ecd368f2c7e3d0385dd90abd
Lokomolo <marcinhenzel96@gmail.com>
2020-02-13 21:53:00 +0100

    add mileage template component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,12 +3,12 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule, MatButtonModule, MatTabsModule, MatListModule } from '@angular/material';
+import { MatSelectModule, MatButtonModule, MatTabsModule, MatListModule, MatRadioModule } from '@angular/material';
 import { MatTableModule } from '@angular/material/table';
 import { MatDividerModule } from '@angular/material/divider';
 
 
-const modules: Array<any> = [MatDividerModule, MatTableModule, MatDatepickerModule, MatNativeDateModule,
+const modules: Array<any> = [MatRadioModule, MatDividerModule, MatTableModule, MatDatepickerModule, MatNativeDateModule,
                             MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatTabsModule, MatListModule];
 @NgModule({
   imports: [...modules],
