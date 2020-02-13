23cb4af23a6278cfadf634317afc5f4b87301349
Lokomolo <marcinhenzel96@gmail.com>
2020-02-13 14:50:20 +0100

    add tabs and search to search office component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,11 +3,13 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule, MatButtonModule } from '@angular/material';
-import {MatTableModule} from '@angular/material/table';
+import { MatSelectModule, MatButtonModule, MatTabsModule, MatListModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
 
 
-const modules: Array<any> = [MatTableModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule];
+const modules: Array<any> = [MatDividerModule, MatTableModule, MatDatepickerModule, MatNativeDateModule,
+                            MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatTabsModule, MatListModule];
 @NgModule({
   imports: [...modules],
   exports: [...modules]
