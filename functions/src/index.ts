48f50874aa0d96e48c6b7dd4fe6424e4ca9da6b4
Lokomolo <marcinhenzel96@gmail.com>
2020-05-30 19:43:18 +0200

    add functions

---
+++ functions/src/index.ts
@@ -0,0 +1,371 @@
+import * as functions from 'firebase-functions';
+const ans = [{
+  delegationNr: '1111delegationNr',
+  name: 'eee',
+  date: 'H',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: '2222delegationNr',
+  name: 'delssNr',
+  date: 'He',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: '3333delegationNr',
+  name: 'ddd',
+  date: 'Li',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: '444delegationNr',
+  name: 'sss',
+  date: 'Be',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: '555delegationNr',
+  name: 'deleaNr',
+  date: 'B',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: 'delegationNr',
+  name: 'ffff',
+  date: 'C',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: '666delegationNr',
+  name: 'dddd',
+  date: 'N',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: 'delegationNr',
+  name: 'aaa',
+  date: 'O',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: '777delegationNr',
+  name: 'ddd',
+  date: 'F',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+},
+{
+  delegationNr: 'delegationNr',
+  name: 'eee',
+  date: 'Ne',
+  destination: 'destination',
+  delegationPlace: 'delegacj',
+  costs: 'costs',
+  advancePay: '123',
+  transport: 'plane'
+}
+];
+const  windows = [{
+  pcName: 'Hydrogen',
+  systemName: 'systemName',
+  activationDate: 'H'
+},
+{
+  pcName: 'Helium',
+  systemName: 'systemName',
+  activationDate: 'He'
+},
+{
+  pcName: 'Lithium',
+  systemName: 'systemName',
+  activationDate: 'Li'
+},
+{
+  pcName: 'Beryllium',
+  systemName: 'systemName',
+  activationDate: 'Be'
+},
+{
+  pcName: 'Boron',
+  systemName: 'systemName',
+  activationDate: 'B'
+},
+{
+  pcName: 'Carbon',
+  systemName: 'systemName',
+  activationDate: 'C'
+},
+{
+  pcName: 'Nitrogen',
+  systemName: 'systemName',
+  activationDate: 'N'
+},
+{
+  pcName: 'Oxygen',
+  systemName: 'systemName',
+  activationDate: 'O'
+},
+{
+  pcName: 'Fluorine',
+  systemName: 'systemName',
+  activationDate: 'F'
+},
+{
+  pcName: 'Neon',
+  systemName: 'systemName',
+  activationDate: 'Ne'
+},
+];
+const  office = [{
+  date: 1,
+  pcName: 'Hydrogen',
+  key: 1.0079,
+  version: 'H'
+},
+{
+  date: 2,
+  pcName: 'Helium',
+  key: 4.0026,
+  version: 'He'
+},
+{
+  date: 3,
+  pcName: 'Lithium',
+  key: 6.941,
+  version: 'Li'
+},
+{
+  date: 4,
+  pcName: 'Beryllium',
+  key: 9.0122,
+  version: 'Be'
+},
+{
+  date: 5,
+  pcName: 'Boron',
+  key: 10.811,
+  version: 'B'
+},
+{
+  date: 6,
+  pcName: 'Carbon',
+  key: 12.0107,
+  version: 'C'
+},
+{
+  date: 7,
+  pcName: 'Nitrogen',
+  key: 14.0067,
+  version: 'N'
+},
+{
+  date: 8,
+  pcName: 'Oxygen',
+  key: 15.9994,
+  version: 'O'
+},
+{
+  date: 9,
+  pcName: 'Fluorine',
+  key: 18.9984,
+  version: 'F'
+},
+{
+  date: 10,
+  pcName: 'Neon',
+  key: 20.1797,
+  version: 'Ne'
+},
+];
+const  vehicle = [{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'H',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'He',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'Li',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'Be',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'B',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'C',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'N',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'O',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'F',
+  costs: 'costs'
+},
+{
+  registrationNr: 'registrationNr',
+  delegationNr: 'delegationNr',
+  mileage: 'Ne',
+  costs: 'costs'
+},
+];
+
+const  costs = ['dsa', 'dsa222'];
+const regs = ['dsa', 'dsa222'];
+const offices = ['dsa', 'dsa222'];
+const windowses = ['dsa', 'dsa222'];
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((req: any,est, res: any)onse) => {
+//  res: any)onse.send("Hello from Firebase!");
+// });
+const express = require("express");
+/* const cors = require('cors'); */
+const admin = require('firebase-admin');
+const app = express();
+const main = express();
+/* const PORT = 3000; */
+admin.initializeApp(functions.config().firebase);
+
+/* var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions)); */
+main.use('/Myapi', app);
+main.use(express.json());
+export const webApi = functions.https.onRequest(main);
+app.post('/sendMessage', (req: any, res: any) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).send({message: 'dsa'});
+})
+app.post('/sendFireAlarm', (req: any, res: any) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).send({message: 'fire'});
+})
+
+app.post('/addWindows', (req: any, res: any) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).send({message: 'dsa'});
+});
+app.post('/addDeleg', (req: any, res: any) => {
+  res.status(200).send({message: 'dsa'});
+});
+app.post('/addOffice', (req: any, res: any) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).send({message: 'dsa'});
+});
+app.post('/addMileage', (req: any, res: any) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).send({message: 'dsa'});
+});
+app.post('/addVehicle', (req: any, res: any) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).send({message: 'dsa'});
+});
+
+
+app.get('/getCosts', (req: any, res: any) => {
+  res.status(200).send(costs);
+});
+app.get('/getRegs', (req: any, res: any) => {
+  res.status(200).send(regs);
+});
+app.get('/getOffices', (req: any, res: any) => {
+  res.status(200).send(offices);
+});
+app.get('/getWindows', (req: any, res: any) => {
+  res.status(200).send(windowses);
+});
+app.get('/search-delegation', (req: any, res: any) => {
+  console.log(req.query);
+  res.status(200).send([...ans, ...ans]);
+});
+app.get('/search-windows', (req: any, res: any) => {
+  console.log(req.query);
+  res.status(200).send([...windows, ...windows]);
+});
+app.get('/search-office', (req: any, res: any) => {
+  console.log(req.query);
+  res.status(200).send([...office, ...office]);
+});
+app.get('/search-vehicle', (req: any, res: any) => {
+  console.log(req.query);
+  res.status(200).send([...vehicle, ...vehicle]);
+});
+
+/* app.listen(PORT, () => {
+  console.log(`server running on dsaport ${PORT}`);
+});
+ */
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'http://localhost:3000'
+  apiUrl:'https://us-central1-itfs-cfcd9.cloudfunctions.net/webApi'
 };
